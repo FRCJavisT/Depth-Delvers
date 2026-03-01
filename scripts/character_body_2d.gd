7ac1088d0471efb9d4698caff734d366b3142c3a
AtharvanT <atami3@ocdsb.ca>
2026-03-01 08:46:20 -0500

    Update character_body_2d.gd

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -17,11 +17,13 @@ var _is_swinging: bool = false
 
 func _ready() -> void:
 	_base_weapon_x = weapon_sprite.position.x
-	if pickableitems.weapon=="Trident":
-		speed=800
 
 func _physics_process(delta):
 	# 1. Get movement input
+	if UserInterface.weapon != null and UserInterface.weapon.name == "Trident":
+		speed = 5000
+	else:
+		speed=600
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
