60aee715770b234e0498858bbc796661d5d2d1a1
FRCJavisT <jtsai2@ocdsb.ca>
2026-03-01 07:52:41 -0500

    Fixed game and fixed trident

--- startinganimation.gd
+++ startinganimation.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
 
+func _ready() -> void:
+	Parallax2d.visible = false
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	transition.transition("res://scenes/level.tscn")
+	Parallax2d.visible = true
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	LayerPopup.show_layer(1)
